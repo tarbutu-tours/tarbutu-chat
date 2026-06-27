c04ce8150aa826d94afdb751a8014ee5f5e218a9
tarbutu-tours <tarbutu1@gmail.com>
2026-06-27 21:41:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
